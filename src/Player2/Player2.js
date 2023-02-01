a1e068a68d39f8eae616e126720f0fedcc463887
Jaroslav Nový <92918228+tymasdost@users.noreply.github.com>
2023-02-01 20:58:10 +0100

    Domácí vánočka
    Z droždí a 50 g cukru uděláme kvásek. Nejdříve rozmícháme droždí s cukrem do tekuta, pak přidáme asi 100 ml teplého mléka a necháme vzejít kvásek.
    Jak udělat kvásek z droždí
    
    V míse nebo v míse kuchyňského robotu smícháme vejce, změklé máslo a cukr. Přidáme sůl, muškátový oříšek a citrónové aroma (nebo citrónovou kůru). Promícháme. Pokud používáme kuchyňský robot, nasadíme hák na hnětení. Střídavě přidáváme mouku, kvásek a zbytek teplého mléka. Vypracujeme hladké a pružné kynuté těsto. Těsto hněteme tak dlouho, aby se nelepilo na stěny. Dle potřeby přidáme mouku, je-li těsto moc řídké, nebo mléko, je-li naopak moc tuhé. Propracované těsto necháme 1 hodinu kynout.
    Po hodině prohněteme a necháme další 1 hodinu kynout.
    Po vykynutí těsto rozdělíme na 9 stejných částí, ze kterých vyválíme stejně dlouhé válečky. Vánočku upleteme, nejprve ze 4, pak 3 a nakonec 2 dílů.
    Připravíme plech, který vyložíme pečícím papírem. Jednotlivě upečené copy vždy dáváme rovnou na plech (tzn. nepřenášíme celou vánočku z pracovní plochy na plech, ale nejdříve na plech dáme cop ze čtyř dílů, pak cop ze tří, zacelíme, nakonec cop ze dvou, opět zacelíme). Aby vánočka "nesjela", můžeme ji propíchat třemi špejlemi. Vánočku přikryjeme utěrkou a necháme na plechu kynout, asi 45 minut.
    Jak uplést vánočku z 9 pramenů
    
    Jakmile je vánočka nakynutá, předehřejeme troubu na 160 °C. Vánočku potřeme rozšlehaným vajíčkem s 1 lžící vody a posypeme plátky mandlí. Pečeme 45 minut. Po upečení vánočku poprášíme moučkovým cukrem. Z jedné dávky bude vánočka hodně velká.

---
+++ src/Player2/Player2.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Player2.css'
+import Card from '../Card/Card';
+
+const PlayedCards = (props) => {
+  let {playedDeck} = props;
+  let itemList=[];
+
+  playedDeck.forEach(()=>{
+    itemList.push(<Card key={Math.random()} value={["card_back_alt",0,"player2"]}  />)
+  });
+
+  return (
+    <>
+    <div id="player2Cards">
+        {itemList}
+    </div>
+    </>
+  );
+};
+
+export default PlayedCards;
